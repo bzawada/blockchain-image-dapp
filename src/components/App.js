042ab1b66681c6b6759f9698875ab72495466c68
Bartek Zawada <krucjo12@gmail.com>
2021-01-13 22:18:27 +0100

    feat: add react components

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,95 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
-import Identicon from 'identicon.js';
 import './App.css';
 import Decentragram from '../abis/Decentragram.json'
 import Navbar from './Navbar'
 import Main from './Main'
 
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https'})
 
 class App extends Component {
+  async componentDidMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    const networkId = await web3.eth.net.getId()
+    const networkData = Decentragram.networks[networkId]
+    if (networkData) {
+      const decentragram = web3.eth.Contract(Decentragram.abi, networkData.address)
+      this.setState({ decentragram })
+      const imageCount = await decentragram.methods.imageCount().call()
+      this.setState({ imageCount })
+
+      for(let i = 1; i <= imageCount; i++) {
+        const image = await decentragram.methods.images(i).call()
+        this.setState({ images: [...this.state.images, image]})
+      }
+
+      this.setState({ images: this.state.images.sort((a,b) => b.tipAmount - a.tipAmount)})
+      this.setState({ loading: false })
+    } else {
+      window.alert('Decentragram contract not deployed to detected network')
+    }
+  }
+
+  captureFile = event => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result)})
+    }
+  }
+
+  uploadImage = description => {
+    ipfs.add(this.state.buffer, (error, result) => {
+      if (error) {
+        console.error(error)
+        return;
+      }
+
+      this.setState({ loading: true})
+      this.state.decentragram.methods.uploadImage(result[0].hash, description).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false})
+      })
+    })
+  }
+
+  tipImageOwner = (id, amount) => {
+      this.setState({ loading: true})
+      this.state.decentragram.methods.tipImageOwner(id).send({ from: this.state.account, value: amount }).on('transactionHash', (hash) => {
+        this.setState({loading: false})
+      })
+  }
 
   constructor(props) {
     super(props)
     this.state = {
       account: '',
+      decentragram: null,
+      images: [],
+      imagesCount: 0,
+      loading: true
     }
   }
 
@@ -22,10 +99,14 @@ class App extends Component {
         <Navbar account={this.state.account} />
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-            // Code...
+          :
+            <Main
+              images={this.state.images}
+              captureFile={this.captureFile}
+              uploadImage={this.uploadImage}
+              tipImageOwner={this.tipImageOwner}
+              account={this.state.account}
             />
-          }
         }
       </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -10,14 +10,68 @@ class Main extends Component {
           <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
             <div className="content mr-auto ml-auto">
               <p>&nbsp;</p>
-              <h1 className="d-4">Edit this file in App.js!</h1>
-                
-                {/* Code ... */}
-
+              <h2>Share image</h2>
+              <form onSubmit={(event) => {
+                event.preventDefault()
+                const description = this.imageDescription.value
+                this.props.uploadImage(description)
+              }}>
+                <input type="file" accept=".jpg, .jpeg, .png, .bmp, .gif" onChange={this.props.captureFile} />
+                <div className="form-group mr-sm-2">
+                  <br></br>
+                  <input
+                    id="imageDescription"
+                    type="text"
+                    ref={(input) => {this.imageDescription = input }}
+                    className="form-control"
+                    placeholder="Image description..."
+                    required
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary btn-block btn-lg">Upload!</button>
+              </form>
               <p>&nbsp;</p>
-                
-                {/* Code ... */}
-
+              { this.props.images.map((image, key) => {
+                return(
+                    <div className="card mb-4" key={key}>
+                      <div className="card-header">
+                        <img
+                          className="mr-2"
+                          width="30"
+                          height="30"
+                          src={`data:image/png;base64,${new Identicon(image.author, 30).toString()}`}
+                        />
+                        <small className="text-muted">{image.author}</small>
+                      </div>
+                      <ul id="imageList" className="list-group list-group-flush">
+                        <li className="list-group-item">
+                          <p className="text-center">
+                            <img src={`https://ipfs.infura.io/ipfs/${image.hash}`} style={{ maxWidth: '420px'}} />
+                          </p>
+                          <p>{image.description}</p>
+                        </li>
+                        <li key={key} className="list-group-item py-2">
+                          <small className="float-left mt-1 text-muted">
+                            TIPS: {window.web3.utils.fromWei(image.tipAmount.toString(), 'ether')} ETH
+                          </small>
+                          { this.props.account !== image.author ?
+                              <button
+                                  className="btn btn-link btn-sm float-right pt-0"
+                                  name={image.id}
+                                  onClick={(event) => {
+                                    let tipAmount = window.web3.utils.toWei('0.1', 'ether')
+                                    this.props.tipImageOwner(event.target.name, tipAmount)
+                                  }}
+                              >
+                                TIP 0.1 ETH
+                              </button>
+                              : <p className="float-right">You are the author</p>
+                          }
+                        </li>
+                      </ul>
+                    </div>
+                )
+              })}
             </div>
           </main>
         </div>
